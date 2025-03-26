20ebf9570bc2f93af55eb0acb84332c0d440faff
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-03-26 14:30:32 +0530

    Load Table in Item View

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -4,6 +4,8 @@
  */
 package edu.ijse.mvc;
 
+import edu.ijse.mvc.view.ItemView;
+
 /**
  *
  * @author anjana
@@ -14,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ItemView().setVisible(true);
     }
     
 }
