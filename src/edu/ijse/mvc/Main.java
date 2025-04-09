eb173666c29a1688377bcbefd8eacf908bab64d0
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-04-09 10:39:10 +0530

    Order View Initiate

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -6,6 +6,7 @@ package edu.ijse.mvc;
 
 import edu.ijse.mvc.view.CustomerView;
 import edu.ijse.mvc.view.ItemView;
+import edu.ijse.mvc.view.OrderView;
 
 /**
  *
@@ -18,7 +19,8 @@ public class Main {
      */
     public static void main(String[] args) throws Exception{
         //new ItemView().setVisible(true);
-        new CustomerView().setVisible(true);
+        //new CustomerView().setVisible(true);
+        new OrderView().setVisible(true);
     }
     
 }
